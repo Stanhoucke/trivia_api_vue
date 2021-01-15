3d895b1837d03c56bdd84f9f2edf163edb6e6e76
Stanley Houcke <stanhoucke@gmail.com>
2021-01-15 15:11:46 +0000

    event bus carries info from form to app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template lang="html">
   <div>
     <h1>Welcome to Vue Open Trivia!</h1>
+    <trivia-form></trivia-form>
     <trivia-all-questions :questions="trivia"></trivia-all-questions>
   </div>
 </template>
 
 <script>
-import TriviaAllQuestions from '@/components/TriviaAllQuestions.vue'
+import { eventBus } from "@/main.js";
+import TriviaAllQuestions from '@/components/TriviaAllQuestions.vue';
+import TriviaForm from '@/components/TriviaForm.vue';
 
 export default {
   name: 'app',
   data(){           
     return {
-      trivia: null
+      trivia: null,
+      newGameInfo: null
     };
   },
   components: {
-    'trivia-all-questions': TriviaAllQuestions
+    'trivia-all-questions': TriviaAllQuestions,
+    'trivia-form': TriviaForm
   },
   mounted(){
     const url = `https://opentdb.com/api.php?amount=10`
     fetch(url)
     .then(res => res.json())
     .then(trivia => this.trivia = trivia.results)
+
+    eventBus.$on('new-game-info', (newGameInfo) => {
+      this.newGameInfo = newGameInfo;
+    })
   }
 
 }
--- src/components/TriviaForm.vue
+++ src/components/TriviaForm.vue
@@ -1,14 +1,27 @@
 <template lang="html">
-  
+  <div>
+      <h3>Choose from the below options to generate a random game of trivia!</h3>
+      <form v-on:submit.prevent="emitNewGameInfo" id="generate-trivia-form">
+      <input type="number" min="0" placeholder="e.g. 10" v-model="numberOfQuestions">
+        <input type="submit" value="Play!">
+      </form>
+  </div>
 </template>
 
 <script>
+import { eventBus } from "@/main.js";
+
 export default {
     name: 'trivia-form',
     data(){
         return{
-
+            numberOfQuestions: null
         };
+    },
+    methods: {
+        emitNewGameInfo: function(){
+            eventBus.$emit('new-game-info', this.numberOfQuestions)
+        }
     }
 
 }
