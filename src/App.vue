a314fa456d67f5a942074300a7a7c14f67f12fdb
Stanley Houcke <stanhoucke@gmail.com>
2021-01-15 12:17:01 +0000

    successful api fetch

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,21 @@
 
 <script>
 export default {
+  name: 'app',
+  data(){
+    return {
+      trivia: null
+    };
+  },
+  components: {
+
+  },
+  mounted(){
+    const url = `https://opentdb.com/api.php?amount=10`
+    fetch(url)
+    .then(res => res.json())
+    .then(trivia => this.trivia = trivia.results)
+  }
 
 }
 </script>
