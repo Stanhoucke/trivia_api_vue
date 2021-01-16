5f1fe03a95013e03cc9b4c9e8d197de8bed46224
Stanley Houcke <stanhoucke@gmail.com>
2021-01-16 12:12:33 +0000

    added more categories

--- src/components/TriviaForm.vue
+++ src/components/TriviaForm.vue
@@ -6,7 +6,7 @@
         <label for="category">Pick a category: </label>
         <select name="category" v-model="newGameInfo.category">
             <option value="">Any</option>
-            <option value="9">General Knowledge</option>
+            <option v-for="(category, i) in categories" :value="category">{{i}}</option>
         </select>
         <label for="difficulty">Difficulty: </label>
         <select name="difficulty" v-model="newGameInfo.difficulty">
@@ -40,7 +40,33 @@ export default {
                 difficulty: "",
                 type: ""
             },
-            showForm: true
+            showForm: true,
+            categories: {
+                "General Knowledge": 9,
+                "Entertainment: Books": 10,
+                "Entertainment: Film": 11,
+                "Entertainment: Music": 12,
+                "Entertainment: Musicals & Theatres": 13,
+                "Entertainment: Television": 14,
+                "Entertainment: Video Games": 15,
+                "Entertainment: Board Games": 16,
+                "Science & Nature": 17,
+                "Science: Computers": 18,
+                "Science: Mathematics": 19,
+                Mythology: 20,
+                Sports: 21,
+                Geography: 22,
+                History: 23,
+                Politics: 24,
+                Art: 25,
+                Celebrities: 26,
+                Animals: 27,
+                Vehicles: 28,
+                "Entertainment: Comics": 29,
+                "Science: Gadgets": 30,
+                "Entertainment: Japanese Anime & Manga": 31,
+                "Entertainment: Cartoon & Animations": 32
+            }
         };
     },
     methods: {
